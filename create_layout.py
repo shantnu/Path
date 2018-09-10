e84dd6be2ac249cf0637ddef50e865bd5811795a
Shantnu Tiwari <shantnutiwari@gmail.com>
2018-09-10 20:47:51 +0100

    fix menu

--- create_layout.py
+++ create_layout.py
@@ -3,6 +3,8 @@ with open("layouts/default.html","r") as f:
 data = data.replace("../css", "css")
 
 data = data.replace("../images", "images")
+data = data.replace("../js", "js")
+
 
 data = data.replace("blossom1", "moon")
 
